1dd2bff2decf183445294b83c3939ed7e01a612e
katherine Villamar <64608973+MeyVillamaldon@users.noreply.github.com>
2024-02-14 13:49:32 -0500

    add page

---
+++ index.js
@@ -0,0 +1,23 @@
+function deseo(){
+
+let opciones = [
+    'Helado gratis',
+    'Dia de aventura',
+    'Ramen o Sushi',
+    'Ir por Bubble-tea',
+    'Pollito frito',
+    'Ir al cine',
+    'Mr joy',
+    'Pista de bolos',
+    'Fotos ramdon'
+];
+let cantidad = opciones.length;
+let ramdon= opciones[Math.floor(Math.random()*cantidad)+1];
+
+//llamo del html donde ira el txt
+let cuponera =document.getElementById('cuponera');
+
+
+cuponera.innerHTML=`
+<p class="cuponera"><br> ${ramdon} </p> `
+}
